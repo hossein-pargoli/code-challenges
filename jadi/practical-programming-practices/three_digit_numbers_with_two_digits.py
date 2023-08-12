7ade522d3312a8bc6e2374544083ab8b8a2a0bae
dev1001 <dev1001.code@gmail.com>
2023-08-12 19:20:36 +0330

    solve a challenge from jadi youtube channel

---
+++ jadi/practical-programming-practices/three_digit_numbers_with_two_digits.py
@@ -0,0 +1,22 @@
+# https://www.youtube.com/watch?v=D48MKRyQZMA
+# https://github.com/jadijadi/Practical-Programming-Practices/tree/main/math/111-999-two-digits-no-zero
+
+# numbers = [number for number in range(100, 1000) if '0' not in str(number)]
+numbers = [number for number in range(100, 1000)]
+counter = 0
+
+
+def check_digits(number: int) -> bool:
+    digits = set()
+    for _ in range(3):
+        digit = number % 10
+        digits.add(str(digit))
+        number //= 10
+    return ('0' not in digits) and (len(digits) == 2)
+
+
+for number in numbers:
+    if check_digits(number):
+        counter += 1
+
+print(counter)
